9782eaf0cf2e689eb86e21c92429d0e07530b7ac
Mason Brown <masubrow@ucsc.edu>
2025-11-30 17:58:13 -0800

    accidentally committed api keys whoops using env variables now

--- web/lib/firebase.ts
+++ web/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 // TODO: Replace with your Firebase project configuration
 // You can find this in Firebase Console -> Project Settings -> General -> Your Apps -> SDK Setup and Configuration -> Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCAZQ4-tOWSc50OKde5yMPvpNOB4uno2dE",
-  authDomain: "twogether-web.firebaseapp.com",
-  projectId: "twogether-web",
-  storageBucket: "twogether-web.firebasestorage.app",
-  messagingSenderId: "44168866754",
-  appId: "1:44168866754:web:7df5cdecb9b046ec3b445b",
-  measurementId: "G-DMZB0GZBFZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
